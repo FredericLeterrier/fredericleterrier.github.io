4fe5d78b0613ec5bd3ec25b22045328234d4bc55
Fredodido <utilisateur@debian>
2016-06-21 11:57:54 +0200

    test

---
+++ js/style.js
@@ -0,0 +1,9 @@
+<script>$(".button-collapse").sideNav();</script>
+
+<script> $(document).ready(function(){
+    $('.slider').slider({full_width: true});
+  });</script>
+
+  <script>$(document).ready(function(){
+      $('.carousel').carousel();
+    });
