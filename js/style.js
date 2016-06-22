14ea75f24932afb78862306ec68be69dade960f3
Fredodido <utilisateur@debian>
2016-06-22 15:07:09 +0200

    rangement dossier javascript

--- js/style.js
+++ js/style.js
@@ -1,9 +1,40 @@
-<script>$(".button-collapse").sideNav();</script>
+// LOADING
+function autograph() {
+    var parent = document.querySelector('.signature'),
+        path = document.querySelector('.signature__path'),
+        length = path.getTotalLength();
+    path.style.transition = path.style.WebkitTransition = '4000';
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    path.getBoundingClientRect();
+    path.style.transition = path.style.webkitTransition = 'stroke-dashoffset 3.75s linear';
+    path.style.strokeDashoffset = '0';
+    path.style.strokeDashoffset = Math.abs(length) * -1;
+};
+setInterval(function() {
+    autograph();
+}, 4000);
+autograph();
 
-<script> $(document).ready(function(){
+// NAV
+$(".button-collapse").sideNav();
+
+//SLIDE MODAL
+ $(document).ready(function(){
     $('.slider').slider({full_width: true});
-  });</script>
+     $('.carousel').carousel();
+     $('.modal-trigger').leanModal();
+     $('#modal1').openModal();
+   $('#modal1').closeModal();
+     $('.carousel').carousel();
+    $('.carousel').carousel('next');
+$('.carousel').carousel('next', [3]);
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', [4]);
+ });
 
-  <script>$(document).ready(function(){
-      $('.carousel').carousel();
+//FLECHE
+ $("#fleche").click(function(){
+        $('html,body').animate({scrollTop: 0},2000);
     });
+
