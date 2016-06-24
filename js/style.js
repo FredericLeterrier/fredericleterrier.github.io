879ccf9d796c04252d04ef0adaeb0142b45a50f0
Fredodido <utilisateur@debian>
2016-06-24 11:11:36 +0200

    rangement

--- js/style.js
+++ js/style.js
@@ -1,24 +1,24 @@
 
 
 // NAV
-$(".button-collapse").sideNav();
+		$(".button-collapse").sideNav();
 
 //SLIDE MODAL
  $(document).ready(function(){
-    $('.slider').slider({full_width: true});
-     $('.carousel').carousel();
-     $('.modal-trigger').leanModal();
-     $('#modal1').openModal();
-   $('#modal1').closeModal();
-     $('.carousel').carousel();
-    $('.carousel').carousel('next');
-$('.carousel').carousel('next', [3]);
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', [4]);
+    	$('.slider').slider({full_width: true});
+     	$('.carousel').carousel();
+     	$('.modal-trigger').leanModal();
+     	$('#modal1').openModal();
+  	 	$('#modal1').closeModal();
+     	$('.carousel').carousel();
+    	$('.carousel').carousel('next');
+		$('.carousel').carousel('next', [3]);
+		$('.carousel').carousel('prev');
+		$('.carousel').carousel('prev', [4]);
  });
 
 //FLECHE
- $("#fleche").click(function(){
+ 		$("#fleche").click(function(){
         $('html,body').animate({scrollTop: 0},2000);
     });
 
